8d0d66145a1fa990c2008595f44b0add9835df08
Reggy G <rgranovskiy@gmail.com>
2017-02-16 11:00:19 -0500

    More features (face sentiment detection and safe search detection)

--- extracttext.py
+++ extracttext.py
@@ -1,5 +1,5 @@
-import base64
-
+"""Basic applications using the Google Vision API"""
+import base64, sys
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
@@ -7,25 +7,53 @@ from oauth2client.client import GoogleCredentials
 credentials = GoogleCredentials.get_application_default()
 service = discovery.build('vision', 'v1', credentials=credentials)
 
-with open('book.jpg', 'rb') as image:
-    img = base64.b64encode(image.read()) #encode the binary for JSON
+def imagecontent(image): #encode image for processing
+    return base64.b64encode(open(image, 'rb').read())
+
+
+def getfeatures(image): #extract the text from an image
     #JSON for wrapper for HTTP POST request for text features
     service_request = service.images().annotate(body={
         'requests':[
             {
                 'image':{
-                    'content': img.decode('UTF-8')
-                },
-            'features':[{
-                'type': 'TEXT_DETECTION',
-                'maxResults': 12
-             }
-             ]
-            }
-        ]
-    })
-    #execute the request and get a response to process
+                    'content':imagecontent(image).decode('UTF-8')
+                    },
+                'features':[
+                    {
+                        'type': 'TEXT_DETECTION',
+                        'maxResults': 12
+                    },
+                    {
+                        'type': 'SAFE_SEARCH_DETECTION'
+                    },
+                    {
+                        'type': 'FACE_DETECTION'
+                    }
+                    ]
+                }
+            ]
+        })
+    #execute request and get a response to process
     response = service_request.execute()
-    text = response['responses'][0]['textAnnotations'][0]
-    print("The text language is {} and it says {}".format(text['locale'], text['description']))
-    print(text)
+    faceinfo = None
+    textinfo = None
+    safeinfo = None
+    for feature in response['responses'][0]:
+        if feature == 'textAnnotations':
+            textinfo = response['responses'][0][feature][0]
+        elif feature == 'safeSearchAnnotation':
+            safeinfo = response['responses'][0][feature]
+        elif feature == 'faceAnnotations':
+            faceinfo = response['responses'][0][feature][0]
+    return textinfo, faceinfo, safeinfo
+
+textinfo, faceinfo, safeinfo = getfeatures(sys.argv[1])
+if textinfo is not None:
+    print("There is text in language {} that says {}".format(textinfo['locale'], textinfo['description']))
+if faceinfo is not None:
+    print("Joy: {} \nSorrow: {} \nAnger: {} \nSurprise: {}".format(faceinfo['joyLikelihood'], faceinfo['sorrowLikelihood'], faceinfo['angerLikelihood'], faceinfo['surpriseLikelihood']))
+if safeinfo is not None:
+    print("Adult content is {} \nSpoof is {} \nMedical content is {} \nViolent content is {}".format(safeinfo['adult'], safeinfo['spoof'], safeinfo['medical'], safeinfo['violence']))
+
+
